0adc31debba25aba780344d30c0c0badcf3d4ad6
Evan Schalton <Evan.Schalton@Gmail.Com>
2019-03-18 20:22:15 -0400

    update package folder dir

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 
 setup(
     name='HTMLr',
-    version='0.1dev',
-    packages=['htmlr',],
+    version='0.3dev',
+    packages=['HTMLr',],
     license='Do whatever you want!',
     long_description=open('README.md').read(),
 )
