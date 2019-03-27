34ba7ccdd21f60f92cb1b1d63a35803d20937db2
Evan Schalton <Evan.Schalton@Gmail.Com>
2019-03-27 16:43:42 -0400

    added 'HTMLr.templates' to build file

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 
 setup(
     name='HTMLr',
-    version='0.9.0-dev',
-    packages=['HTMLr',],
+    version='0.9.1-dev',
+    packages=['HTMLr', 'HTMLr.templates'],
     license='Do whatever you want!',
     long_description=open('README.md').read(),
 )
