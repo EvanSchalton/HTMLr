4f64a2378cb8441c0b6fbe45150281fc33db70f5
Evan Schalton <Evan.Schalton@Gmail.Com>
2019-03-27 16:39:09 -0400

    rebuilt after fixing capitalization error

--- htmlr/__init__.py
+++ htmlr/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '0.8.8-dev'
+__version__ = '0.9.0-dev'
 # from HTMLr.core import HTMLObject
 # from HTMLr.templates import Card, Combos, Table
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='HTMLr',
-    version='0.8.8-dev',
+    version='0.9.0-dev',
     packages=['HTMLr',],
     license='Do whatever you want!',
     long_description=open('README.md').read(),
