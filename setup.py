91eb1d1fdd631ecffaa9f680f973c619c146a7cf
Evan Schalton <Evan.Schalton@Gmail.Com>
2019-03-27 15:45:54 -0400

    relative imports

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='HTMLr',
-    version='0.8.3-dev',
+    version='0.8.4-dev',
     packages=['HTMLr',],
     license='Do whatever you want!',
     long_description=open('README.md').read(),
